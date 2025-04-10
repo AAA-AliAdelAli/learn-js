f35aa3b11611c167004feec539571304293c7904
Ali Adel Ali <aliafel357@gmail.com>
2025-04-10 02:24:47 +0200

    first commit

---
+++ todoList/main.js
@@ -0,0 +1,44 @@
+let todoList = [];
+renderTodoList();
+
+function renderTodoList(){
+
+
+    let toHtml ='';
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        //const name = todoObject.name ;
+        //const dueDate = todoObject.dueDate;
+        const {name ,dueDate} = todoObject ;
+        const html = `
+            <div> ${name}</div>
+            <div> ${dueDate}</div>
+            <button onclick="
+            todoList.splice(${i} , 1);
+            renderTodoList();
+            " class="delete-todo">Delete</button>
+        `;
+        toHtml +=html;
+        
+
+    }
+    document.querySelector('.js-div').innerHTML = toHtml ;
+}
+function addTodo(){
+    const inputElement = document.querySelector('.js-name-input');
+    const dateInputElement = document.querySelector('.js-date-input')
+    const name = inputElement.value;
+    const dueDate = dateInputElement.value;
+    todoList.push(
+        {
+            // name : name,
+            // dueDate : dueDate ,
+            name,
+            dueDate
+
+        });
+    // console.log(todoList);
+    inputElement.value = '';
+    renderTodoList();
+}
